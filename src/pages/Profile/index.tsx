18ceba51c01c855773ff6f8e2c015794574096ea
oleksiy <oleksiy.oleksiy.askerovych@gmail.com>
2024-08-29 00:49:51 +0300

    Add profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+
+function Profile() {
+  return (
+    <div className={styles.container}>Profile</div>
+  )
+}
+
+export default Profile
