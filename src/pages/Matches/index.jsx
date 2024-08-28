190aedef5edb0b660b6dbcfe9e10a6daa149e0e8
oleksiy <oleksiy.oleksiy.askerovych@gmail.com>
2024-08-28 21:10:41 +0300

    Created Matches component

---
+++ src/pages/Matches/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+
+function Matches() {
+  return (
+    <div className={styles.container}>Matches</div>
+  )
+}
+
+export default Matches
